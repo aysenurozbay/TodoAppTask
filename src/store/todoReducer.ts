5211bb439ba0ff2c06d7cfd587c6dca4020c74df
aysenurozbay <aysenurozbay1004@gmail.com>
2023-12-18 21:12:24 +0300

    redux store cleaned

--- src/store/todoReducer.ts
+++ src/store/todoReducer.ts
@@ -1,4 +1,4 @@
-import {PayloadAction, createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {TodoType} from '../Types';
 import {
   getData,
@@ -10,13 +10,11 @@ import {
 export interface TodoReducerState {
   todos: TodoType[];
   selectedTodo?: TodoType | null;
-  filteredTodo?: TodoType[] | null;
 }
 
 const initialState: TodoReducerState = {
   todos: [],
   selectedTodo: null,
-  filteredTodo: [],
 };
 export const fetchInitialData = createAsyncThunk(
   'waterTracker/fetchInitialData',
@@ -69,36 +67,7 @@ export const deleteAsyncStorageData = createAsyncThunk(
 export const todoSlice = createSlice({
   name: 'todo',
   initialState,
-  reducers: {
-    addTodo: (state, action: PayloadAction<{todo: TodoType}>) => {
-      const newTodo = action.payload.todo;
-      state.todos.push(newTodo);
-    },
-    setSelectedProduct: (state, action: PayloadAction<{id: string}>) => {
-      const todoId = action.payload.id;
-      state.selectedTodo = state.todos.find(p => p.id === todoId);
-    },
-    filterByCategory: (state, action: PayloadAction<{category: string}>) => {
-      const category = action.payload.category;
-      state.filteredTodo = state.todos.filter(p => p.category === category);
-    },
-    filterByStatus: (state, action: PayloadAction<{status: string}>) => {
-      const status = action.payload.status;
-      state.filteredTodo = state.todos.filter(p => p.status === status);
-      console.log('filterByStatus--->', state.filteredTodo);
-    },
-    changeTodoStatus: (
-      state,
-      action: PayloadAction<{id: string; newStatus: TodoType['status']}>,
-    ) => {
-      const todoId = action.payload.id;
-      const status = action.payload.newStatus;
-      const todo = state.todos.find(item => item.id === todoId);
-      if (todo) {
-        todo.status = status;
-      }
-    },
-  },
+  reducers: {},
   extraReducers: builder => {
     builder.addCase(fetchInitialData.fulfilled, (state, action) => {
       if (action.payload) {
